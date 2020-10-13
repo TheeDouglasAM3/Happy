ba3c9509a9283dd1303d7ed817bb7ae31dd7e5c8
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-12 22:32:45 -0300

    Update TileLayer in OrphanatesMap page

--- web/src/pages/OrphanatesMap.tsx
+++ web/src/pages/OrphanatesMap.tsx
@@ -38,8 +38,8 @@ function OrphanatesMap() {
         zoom={12}
         style={{ width: '100%', height: '100%' }}
       >
-        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> 
+        {/* <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} /> */}
       </Map>
 
       <Link to="#" className="create-orphanage">
