db7ca407fb934471eabe6f94bdb809e28c770870
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-14 16:48:06 -0300

    Update routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Landing from './pages/Landing'
-import OrphanatesMap from './pages/OrphanatesMap'
+import OrphanagesMap from './pages/OrphanagesMap'
+import Orphanage from './pages/Orphanage'
+import CreateOrphanage from './pages/CreateOrphanage'
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={Landing} exact/>
-        <Route path="/app" component={OrphanatesMap}/>
+        <Route path="/app" component={OrphanagesMap}/>
+
+        <Route path="/orphanages/create" component={CreateOrphanage}/>
+        <Route path="/orphanages/:id" component={Orphanage}/>
       </Switch>
     </BrowserRouter>
   )
