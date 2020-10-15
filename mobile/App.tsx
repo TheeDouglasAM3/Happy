76d6ed269c028946b61e82acbd1d7762581502d0
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-15 18:18:29 -0300

    Update font of texts in map screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons'
+import { useFonts } from 'expo-font'
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
 
 import mapMarker from './src/images/map-marker.png'
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Nunito_600SemiBold, 
+    Nunito_700Bold, 
+    Nunito_800ExtraBold
+  });
+
+  if(!fontsLoaded) {
+    return null
+  }
   return (
     <View style={styles.container}>
       <MapView 
@@ -76,6 +86,7 @@ const styles = StyleSheet.create({
   calloutText: {
     color: '#0089A5',
     fontSize: 14,
+    fontFamily: 'Nunito_700Bold'
   },
 
   footer: {
@@ -97,6 +108,7 @@ const styles = StyleSheet.create({
   }, 
 
   footerText: {
+    fontFamily: 'Nunito_700Bold',
     color: '#8FA7B3'
   },
 
