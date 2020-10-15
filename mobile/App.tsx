a2d5cb673a52e5c75c48be1688784b6156a8f157
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-15 17:42:06 -0300

    Create map screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons'
+
+import mapMarker from './src/images/map-marker.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude: -23.4494709,
+          longitude: -46.5286301,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
+      >
+        <Marker 
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.4,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude: -23.4494709,
+            longitude: -46.5286301
+          }}
+        >
+          <Callout 
+            tooltip={true}
+            onPress={() => {}}
+          >
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+        <TouchableOpacity 
+          style={styles.createOrphanageButton}
+          onPress={() => {}}
+        >
+          <Feather name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,8 +57,56 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
     justifyContent: 'center',
   },
+
+  calloutText: {
+    color: '#0089A5',
+    fontSize: 14,
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3
+  }, 
+
+  footerText: {
+    color: '#8FA7B3'
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15C3D6',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
