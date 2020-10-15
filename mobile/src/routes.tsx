00cb8fabd349dd5e80056c2fa8730720476c3a83
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-15 18:49:26 -0300

    Update routes

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -4,14 +4,16 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import OrphanagesMap from './pages/OrphanagesMap'
+import OrphanageDetails from './pages/OrphanageDetails'
 
 const { Navigator, Screen } = createStackNavigator()
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Navigator>
+      <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen name="OrphanageDetails" component={OrphanageDetails} />
       </Navigator>
     </NavigationContainer>
   )
