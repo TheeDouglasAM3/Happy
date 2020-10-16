4a3ee5ab94712c636ef064699548a8bc46c230a9
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-16 18:01:11 -0300

    Add quantity orphanages founded in OrphanagesMap

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -60,7 +60,6 @@ export default function OrphanagesMap() {
                 latitude: orphanage.latitude,
                 longitude: orphanage.longitude
               }}
-              
             >
               <Callout 
                 tooltip={true}
@@ -76,7 +75,7 @@ export default function OrphanagesMap() {
       </MapView>
 
       <View style={styles.footer}>
-        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+        <Text style={styles.footerText}>{orphanages.length} orfanatos encontrados</Text>
         <RectButton 
           style={styles.createOrphanageButton}
           onPress={handleNavigateToCreateOrphanage}
