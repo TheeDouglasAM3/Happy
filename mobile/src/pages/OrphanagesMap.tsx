9d1003d21cb488daa12a1af31d7bc2fd98bf985b
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-16 16:52:36 -0300

    Add navigation in screens in OrphanagesMap screen

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import mapMarker from '../images/map-marker.png'
 
@@ -11,6 +12,10 @@ export default function OrphanagesMap() {
 
   function handleNavigateToOrphanageDetails() {
     navigation.navigate('OrphanageDetails')
+  }
+
+  function handleNavigateToCreateOrphanage() {
+    navigation.navigate('SelectMapPosition')
   }
 
   return (
@@ -49,12 +54,12 @@ export default function OrphanagesMap() {
 
       <View style={styles.footer}>
         <Text style={styles.footerText}>2 orfanatos encontrados</Text>
-        <TouchableOpacity 
+        <RectButton 
           style={styles.createOrphanageButton}
-          onPress={() => {}}
+          onPress={handleNavigateToCreateOrphanage}
         >
           <Feather name="plus" size={20} color="#FFF" />
-        </TouchableOpacity>
+        </RectButton>
       </View>
     </View>
   )
