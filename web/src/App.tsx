1a964b2bf01e938ae653cad1c15c0b1d6177c216
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-18 19:22:30 -0300

    Config themes in App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import SwitchTheme from './components/SwitchTheme'
+
+import light from './styles/themes/light'
+import dark from './styles/themes/dark'
 
 import Routes from './routes'
 
-import './styles/global.css'
+import GlobalStyle from './styles/global'
 import 'leaflet/dist/leaflet.css'
-
+import usePersistedState from './utils/usePersistedState'
 
 function App() {
+  const [theme, setTheme] = usePersistedState('theme', light)
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light)
+  }
+
   return (
-    <Routes />
+    <ThemeProvider theme={theme}>
+     <SwitchTheme toggleTheme={toggleTheme} />
+      <GlobalStyle />
+      <Routes />
+    </ThemeProvider>
   )
 }
 
