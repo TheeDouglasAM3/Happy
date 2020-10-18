1dd1f0eebdfd8f701305fc6c1de69b03217c05f9
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-18 19:20:22 -0300

    Create light and dark theme configs

---
+++ web/src/styles/themes/dark.ts
@@ -0,0 +1,15 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    backgroundStart: '#333',
+    backgroundEnd: '#111',
+    backgroundForm: '#222',
+    backgroundBehindContent: '#252525',
+    button: '#CCC',
+    buttonHover: '#EEE',
+    secundaryButton: '#666',
+    secundaryButtonHover: '#888',
+    text: '#DDD'
+  }
+}
---
+++ web/src/styles/themes/light.ts
@@ -0,0 +1,15 @@
+export default {
+  title: 'light',
+
+  colors: {
+    backgroundStart: '#29B6D1',
+    backgroundEnd: '#00C7C7',
+    backgroundForm: '#FFF',
+    backgroundBehindContent: '#EBF2F5',
+    button: '#FFD666',
+    buttonHover: '#FFE293',
+    secundaryButton: '#15C3D6',
+    secundaryButtonHover: '#17D6EB',
+    text: '#FFF'
+  }
+}
