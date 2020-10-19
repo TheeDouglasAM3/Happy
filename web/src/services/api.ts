b15fe2b2adc3a7a21c6f7ed31d810ea1845bc053
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-19 19:15:10 -0300

    Fix baseURL in api.ts

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://happyguarulhos.herokuapp.com/'
+  baseURL: 'https://happyguarulhos.herokuapp.com'
   // baseURL: 'http://localhost:3333'
 })
 
